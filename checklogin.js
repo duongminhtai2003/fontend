b9ab0258aae4e76a7cffe59d3642aefd36f0ccb2
Dương Minh Tài <duongminhtai2003@gmail.com>
2020-04-27 17:35:11 +0700

    tai commit

--- checklogin.js
+++ checklogin.js
@@ -1,8 +1,10 @@
-function checkLogin(){
+function checkLogin() {
     var username = document.getElementById("username").value;
     var password = document.getElementById("password").value;
-    if ( username == "admin" && password == "123456"){
-        alert ("Login successfully");
-        window.location = "/home.html";
+    if (username == "" || password == "") {
+        alert('Please username and password');
+    } else {
+        alert('Login success');
+        window.location = "home.html";
     }
 }
